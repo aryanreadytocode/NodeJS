8359e6049fdd395e8b1e58c3e901f0858b6b7760
Raj Aryan <arpittaryan2k12@gmail.com>
2025-01-29 23:14:34 +0530

    commit

--- 5.5 REST APIs/solution.js
+++ 5.5 REST APIs/solution.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+const yourBearerToken = "3b38abcf-294c-45f9-b22c-d7e79505ddce";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
